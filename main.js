fb1671ce307c411925180958053312cfbd313a33
Emiliano Marchese <e.marchese1991@gmail.com>
2022-10-18 10:25:15 -0300

    Ejercicio 1

--- main.js
+++ main.js
@@ -5,20 +5,28 @@ class Usuario{
         this.libros = libros;
         this.mascotas = mascotas;
     }
-    getFullName(){
-        console.log(`${nombre} ${apellido}`)
+    getFullName(nombre, apellido){
+        return `Full name: ${this.nombre} ${this.apellido}`
     }
     countMascotas(){
-        let cantidadMascotas = length.mascotas;
-        console.log(cantidadMascotas);
+        return ("Cantidad de mascotas:" + Number(this.mascotas.length))
     }
-    addMascota(){
-
+    addMascota(mascotas){
+        this.mascotas.push(mascotas)
     }
-    addBook(){
-
+    addBook(bookName, author){
+        this.libros.push({ bookName, author })
     }
     getBookNames(){
-
+        return `Libro: ${this.bookName}. 
+        Autor: ${this.author}`
     }
 }
+
+const test = new Usuario("emiliano", "marchese", ["Harry potter y el caliz de fuego", "JK Rowling"], "gata")
+
+test.countMascotas;
+test.addBook("1984", "Jeorge Orwell");
+test.addBook("Un Mundo Feliz", "Aldous Huxley");
+
+console.log(test)
